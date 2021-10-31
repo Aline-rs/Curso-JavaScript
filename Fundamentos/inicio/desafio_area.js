d26ea13630737ddbd8668dc75481baf9906f9bde
Aline-rs <alinerosa_s@hotmail.com>
2021-10-31 19:06:35 -0300

    Alguns desafios

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// Formula calculo área ->  PI * raio * raio
+
+let raio = 10;
+const PI = 3.14;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é: " + areaCirc +"m²");
+
+areaCirc = Math.PI * raio * raio;
+
+console.log("O valor da área é: " + areaCirc +"m²");
