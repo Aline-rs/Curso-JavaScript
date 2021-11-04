02f2b29eeb25660b767fae8bc537f9e3d7b7b7f8
Aline-rs <alinerosa_s@hotmail.com>
2021-11-04 19:59:15 -0300

    Adicionando e renomeando arquivos

---
+++ Fundamentos-Programacao/inicio/bloco.js
@@ -0,0 +1,13 @@
+// blocos de código -> AGRUPAMENTOS DE SENTENÇAS!
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+
+console.log("Fim!");
