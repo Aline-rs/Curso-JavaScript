70c435cb819456eab392e6c354be83db4b61d604
Aline-rs <alinerosa_s@hotmail.com>
2021-11-07 18:25:40 -0300

    Anotação dos estudos

---
+++ Fundamentos-JS/organizacao.js
@@ -0,0 +1,6 @@
+console.log("Sentença de código"); // No JavaScript não é obrigatóro o uso do ; | Se decidir usar em uma sentença use em todo o código.
+
+{ //Bloco de código.
+    console.log("Olá!")
+    console.log("Mundo!")
+}
